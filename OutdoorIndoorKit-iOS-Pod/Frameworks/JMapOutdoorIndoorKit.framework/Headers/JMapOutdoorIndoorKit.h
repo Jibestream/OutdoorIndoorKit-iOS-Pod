3f678ba3b098a17339b9df7cf0d4900b912b67d8
developer <developer@jibestream.com>
2019-06-18 17:43:19 +0000

    Travis-CI POD Update: 32

---
+++ OutdoorIndoorKit-iOS-Pod/Frameworks/JMapOutdoorIndoorKit.framework/Headers/JMapOutdoorIndoorKit.h
@@ -0,0 +1,12 @@
+//
+//  JMapOutdoorIndoorKit.h
+//  JMapOutdoorIndoorKit
+//
+//  Created by Louie Yuen on 2019-06-03.
+//  Copyright © 2019 Jibestream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <JMapOutdoorIndoorKit/JMapGMController.h>
+#import <JMapOutdoorIndoorKit/JMapGMMap.h>
+#import <JMapOutdoorIndoorKit/JMapGMGeometry.h>
